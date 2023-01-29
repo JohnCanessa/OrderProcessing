4431d873c042b449604db20ebfacd4dd691fe23e
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2023-01-29 09:41:18 -0600

    First commit

---
+++ IDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockDB
+{
+
+    /// <summary>
+    /// Methods to be mocked.
+    /// </summary>
+    public interface IDBContext
+    {
+        // **** method to be mocked ****
+        Order GetNextOrderDetailFromDB(int orderID);
+
+        // **** method to be mocked ****
+        void SaveOrder(Order nextOrder);
+    }
+}
---
+++ OrderProcessing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockDB
+{
+    /// <summary>
+    /// The example in the article is an Order Processing class, 
+    /// mimicking the order processing system. 
+    /// The business logic in this class is to fetch an order from a database (by order Id), 
+    /// adding 10% GST (Goods and Services Tax) on the amount, 
+    /// and then saving it back to the database.
+    /// </summary>
+    public class OrderProcessing
+    {
+
+        // **** object responsible for database operation ****
+        private IDBContext _dbContext;
+
+
+        /// <summary>
+        /// Reads the order from the database.
+        /// Calculates and updates the Amount by the GST (10%).
+        /// Saves the updated record in the database.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order ProcessGSTForNextOrder(IDBContext dbContext, int orderId)
+        {
+
+            // **** ****
+            _dbContext = dbContext;
+
+            // **** get order by orderId ****
+            var nextOrder = dbContext.GetNextOrderDetailFromDB(orderId);
+
+            // **** calculate and update the order amount ****
+            nextOrder.Amount = CalculateTotalAmountWithGST(nextOrder);
+
+            // **** save the updated order in the database ****
+            _dbContext.SaveOrder(nextOrder);
+
+            // **** return the updated order ****
+            return nextOrder;
+        }
+
+
+        /// <summary>
+        /// This method calclates the updated amount for the specified order.
+        /// The amount is incremented by 10%.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public decimal CalculateTotalAmountWithGST(Order order)
+        {
+            return order.Amount + ((order.Amount * 10) / 100);
+        }
+    }
+}
